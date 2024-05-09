bba3e40148e7ce8ef9f0ecde7c0f0d7e4acd4487
David <davidcastellobatet@gmail.com>
2024-05-09 07:39:47 +0200

    Category List

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-//todo HEADER Component
-//todo FILTER component
-//todo GIFLIST component
-//todo GIFITEM component
-
+import { useState } from 'react';
 import { Header } from './components';
 
 export const App = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+  console.log(categories);
+
   return (
     <>
       <Header />
+      <main>
+        <ol>
+          {categories.map((category, i) => {
+            return <li key={i}>{category}</li>;
+          })}
+        </ol>
+      </main>
     </>
   );
 };
